950b346e811159d0332ec3d0d221dd972ed59b30
avik067 <avikbiswas067>
2023-07-24 14:32:49 +0000

    mid

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,11 +1,13 @@
 // Write your code here
 
+import './index.css'
+
 const NotFound = () => {
   console.log('Not found')
 
   return (
-    <div>
-      <h1>Page Not Found !!</h1>
+    <div className="main-no row-nor center">
+      <h1 className="not-found ">Page Not Found !!</h1>
     </div>
   )
 }
